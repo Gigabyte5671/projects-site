eeea41008cc736c7a5cfaa73aa6968323ec05611
Giga <gigabyte5671@gmail.com>
2021-10-12 17:33:28 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+var ls = new lightshard;
+
+
+ls.init();
+
 function populateDOM(){
-  
+  projects.forEach(function(p){
+    $("projects").append(
+      ls.blocks.general("project_card", "", 
+        ls.blocks.img(p.image) +
+        ls.blocks.general("project_card_inner", "", 
+          ls.blocks.h2(p.title) +
+          ls.blocks.info(p.date) +
+          ls.blocks.paragraph(p.desc)
+        )
+      )
+    );
+  });
 }
+populateDOM();
