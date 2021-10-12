81c5a6abf9e116d45f81af5b9779c84880c563fb
Giga <gigabyte5671@gmail.com>
2021-10-12 17:33:06 +1300

    Create projects.js

---
+++ assets/data/projects.js
@@ -0,0 +1,42 @@
+var projects = [
+  {
+    title: "AutoType JS",
+    tags: ["JavaScript"],
+    date: "2021/10/11",
+    desc: "A simple auto-typing/typewriter effect written in vanilla JS.",
+    image: "https://gigabyte5671.github.io/AutoType/cover.webp",
+    link: ""
+  },
+  {
+    title: "DOM-Particles",
+    tags: ["JavaScript"],
+    date: "2021/09/28",
+    desc: "A simple particle engine that uses JavaScript to run within the native HTML DOM.",
+    image: "https://gigabyte5671.github.io/DOM-Particles/cover.webp",
+    link: ""
+  },
+  {
+    title: "wilderzone Live",
+    tags: [""],
+    date: "2021/08/21",
+    desc: "The main community website for the Tribes: Ascend video game.",
+    image: "https://wilderzone.live/assets/images/site_cover.webp",
+    link: "https://wilderzone.live/"
+  },
+  {
+    title: "wilderzone Servers",
+    tags: [""],
+    date: "2021/08/16",
+    desc: "The fastest way to check who's playing Tribes: Ascend and what game servers are active.",
+    image: "https://servers.wilderzone.live/assets/images/site_cover.webp",
+    link: "https://servers.wilderzone.live/"
+  },
+  {
+    title: "CookieManager JS",
+    tags: [""],
+    date: "2021/05/13",
+    desc: "A simple and lightweight library for adding, fetching, modifying, and removing browser cookies.",
+    image: "assets/images/cookieManager_cover.webp",
+    link: "https://gigabyte5671.github.io/CookieManager/"
+  }
+];
