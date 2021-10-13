8849e7bfe548270aff4d9983bcc95a2b7a8a9770
Giga <gigabyte5671@gmail.com>
2021-10-13 16:32:34 +1300

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,27 @@ ls.init();
 
 function populateDOM(){
   projects.forEach(function(p){
-    $("projects").append(
-      ls.blocks.general("project_card", "", 
-        ls.blocks.img(p.image) +
-        ls.blocks.general("project_card_inner", '" href="#', 
-          ls.blocks.taglist("", p.tags) +
-          ls.blocks.h2(p.title) +
-          ls.blocks.info(p.date) +
-          ls.blocks.paragraph(p.desc)
-        )
-      )
+    $("main").append(
+      ls.blocks.h1(p.title + ":") +
+      '<projects id="' + p.title + '"></projects>'
     );
+    
+
+    p.items.forEach(function(i){
+      $("#" + p.title).append(
+        ls.blocks.link(i.link, "", "", "",
+          ls.blocks.general("project_card", "", 
+            ls.blocks.img(i.image) +
+            ls.blocks.general("project_card_inner", '" href="#', 
+              ls.blocks.taglist("", i.tags) +
+              ls.blocks.h2(i.title) +
+              ls.blocks.info((i.date == "In Progress" ? i.date : ls.toSpokenDate(i.date))) +
+              ls.blocks.paragraph(i.desc)
+            )
+          )
+        )
+      );
+    });
   });
 }
 populateDOM();
@@ -41,3 +51,4 @@ function landingScroll(){
   });
 }
 landingScroll();
+
